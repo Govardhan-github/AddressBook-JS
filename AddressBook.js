2e0e4f416ea9f4a47beffa4da307b720a303e895
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-08-30 19:08:35 +0530

    ADDED : Delete Contact

--- AddressBook.js
+++ AddressBook.js
@@ -102,16 +102,27 @@ let editContact = () => {
     let frstName = prompt("Enter First Name : ");
     let lstName = prompt("Enter Lastt Name : ");
     let index = addressBookArr.findIndex(contact => contact.firstName == frstName && contact.lastName == lstName);
-    if (index == -1)
+    if (index == 0)
         console.log("Could not find the contact!!")
     else {
         addressBookArr[index] = getContact();
         console.log("Contact edited successfully!!");
     }
+}    
+let deleteContact = () => {
+    let frstName = prompt("Enter First Name : ");
+    let lstName = prompt("Enter Last Name : ");
+    let index = getindexByName(frstName, lstName);
+        if (index == -1)
+            console.log("Could not find the contact!!")
+        else {
+            console.log("Contact deleted successfully!!");
+        return addressBookArr.splice(index, 1);
+    }
 }
 let choice = 0;
 do {
-    console.log("Choose\n1. View Contacts\n2. Add Contact\n3. Edit Contact By name\n4. Exit");
+    console.log("Choose\n1. View Contacts\n2. Add Contact\n3. Edit Contact By name\n4. Delete Contact\n5. Exit");
     choice = prompt("Enter Your Choice ");
     switch (choice) {
     case "1": viewContacts();
@@ -120,9 +131,11 @@ do {
         break;
     case "3": editContact();
         break;
-    case "4": 
+    case "4": console.log(deleteContact());
         break;
+    case "5":
+        break;    
     default: console.log("Invalid Choice !!");
     }
 
-} while (choice != 4)
+} while (choice != 5)
